6b89124614681c1cca3a9dfc79ba52e27c4ebbd2
EC2 Default User <ec2-user@ip-172-31-6-17.ec2.internal>
2019-03-24 16:02:04 +0000

    Add compiler and startup scripts

---
+++ compiler.sh
@@ -0,0 +1,4 @@
+javac ~/cnvproject/pt/ulisboa/tencico/cnv/generator
+javac ~/cnvproject/pt/ulisboa/tencico/cnv/server
+javac ~/cnvproject/pt/ulisboa/tencico/cnv/solver
+javac ~/cnvproject/pt/ulisboa/tencico/cnv/util
