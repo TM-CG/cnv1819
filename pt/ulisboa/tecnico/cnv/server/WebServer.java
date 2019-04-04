82b9c9e81be9ca12811aedf97c9fb614d0b5c9fa
Vítor Nunes <vitor.sobrinho.nunes@gmail.com>
2019-04-04 17:02:15 +0100

    Server is now multithreaded
    Code was not been tested!

--- pt/ulisboa/tecnico/cnv/server/WebServer.java
+++ pt/ulisboa/tecnico/cnv/server/WebServer.java
@@ -44,89 +44,101 @@ public class WebServer {
 
 	static class MyHandler implements HttpHandler {
 		@Override
-		public void handle(final HttpExchange t) throws IOException {
+		public void handle(final HttpExchange t) {
 
-			// Get the query.
-			final String query = t.getRequestURI().getQuery();
+			Thread thread = new Thread() {
+				public void run() {
 
-			System.out.println("> Query:\t" + query);
+					try {
+						// Get the query.
+						final String query = t.getRequestURI().getQuery();
 
-			// Break it down into String[].
-			final String[] params = query.split("&");
+						System.out.println("> Query:\t" + query);
 
-			// Store as if it was a direct call to SolverMain.
-			final ArrayList<String> newArgs = new ArrayList<>();
-			for (final String p : params) {
-				final String[] splitParam = p.split("=");
-				newArgs.add("-" + splitParam[0]);
-				newArgs.add(splitParam[1]);
-			}
+						// Break it down into String[].
+						final String[] params = query.split("&");
 
-			newArgs.add("-d");
+						// Store as if it was a direct call to SolverMain.
+						final ArrayList<String> newArgs = new ArrayList<>();
+						for (final String p : params) {
+							final String[] splitParam = p.split("=");
+							newArgs.add("-" + splitParam[0]);
+							newArgs.add(splitParam[1]);
+						}
 
-			// Store from ArrayList into regular String[].
-			final String[] args = new String[newArgs.size()];
-			int i = 0;
-			for(String arg: newArgs) {
-				args[i] = arg;
-				i++;
-			}
+						newArgs.add("-d");
 
-			// Get user-provided flags.
-			final SolverArgumentParser ap = new SolverArgumentParser(args);
+						// Store from ArrayList into regular String[].
+						final String[] args = new String[newArgs.size()];
+						int i = 0;
+						for(String arg: newArgs) {
+							args[i] = arg;
+							i++;
+						}
 
-			// Create solver instance from factory.
-			final Solver s = SolverFactory.getInstance().makeSolver(ap);
+						// Get user-provided flags.
+						final SolverArgumentParser ap = new SolverArgumentParser(args);
 
-			// Write figure file to disk.
-			File responseFile = null;
-			try {
+						// Create solver instance from factory.
+						final Solver s = SolverFactory.getInstance().makeSolver(ap);
 
-				final BufferedImage outputImg = s.solveImage();
+						// Write figure file to disk.
+						File responseFile = null;
+						try {
 
-				final String outPath = ap.getOutputDirectory();
+							final BufferedImage outputImg = s.solveImage();
 
-				final String imageName = s.toString();
+							final String outPath = ap.getOutputDirectory();
 
-				if(ap.isDebugging()) {
-					System.out.println("> Image name: " + imageName);
+							final String imageName = s.toString();
+
+							if(ap.isDebugging()) {
+								System.out.println("> Image name: " + imageName);
+							}
+
+							final Path imagePathPNG = Paths.get(outPath, imageName);
+							ImageIO.write(outputImg, "png", imagePathPNG.toFile());
+
+							responseFile = imagePathPNG.toFile();
+
+						} catch (final FileNotFoundException e) {
+							e.printStackTrace();
+						} catch (final IOException e) {
+							e.printStackTrace();
+						} catch (ClassNotFoundException e) {
+							e.printStackTrace();
+						}
+
+
+
+						// Send response to browser.
+						final Headers hdrs = t.getResponseHeaders();
+
+						t.sendResponseHeaders(200, responseFile.length());
+
+						hdrs.add("Content-Type", "image/png");
+
+						hdrs.add("Access-Control-Allow-Origin", "*");
+						hdrs.add("Access-Control-Allow-Credentials", "true");
+						hdrs.add("Access-Control-Allow-Methods", "POST, GET, HEAD, OPTIONS");
+						hdrs.add("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+
+						final OutputStream os = t.getResponseBody();
+						Files.copy(responseFile.toPath(), os);
+
+
+						os.close();
+
+						System.out.println("> Sent response to " + t.getRemoteAddress().toString());
+						
+				} catch (IOException e) {
+					System.err.println("IOException! Returning thread");
+					return;
 				}
+			}	
+		};
 
-				final Path imagePathPNG = Paths.get(outPath, imageName);
-				ImageIO.write(outputImg, "png", imagePathPNG.toFile());
-
-				responseFile = imagePathPNG.toFile();
-
-			} catch (final FileNotFoundException e) {
-				e.printStackTrace();
-			} catch (final IOException e) {
-				e.printStackTrace();
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			}
-
-
-
-			// Send response to browser.
-			final Headers hdrs = t.getResponseHeaders();
-
-			t.sendResponseHeaders(200, responseFile.length());
-
-			hdrs.add("Content-Type", "image/png");
-
-			hdrs.add("Access-Control-Allow-Origin", "*");
-			hdrs.add("Access-Control-Allow-Credentials", "true");
-			hdrs.add("Access-Control-Allow-Methods", "POST, GET, HEAD, OPTIONS");
-			hdrs.add("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-
-			final OutputStream os = t.getResponseBody();
-			Files.copy(responseFile.toPath(), os);
-
-
-			os.close();
-
-			System.out.println("> Sent response to " + t.getRemoteAddress().toString());
-		}
+		thread.start();
 	}
-
+	}
 }
