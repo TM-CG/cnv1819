7e39a6a39bcc4480dad65d0473b8ff33c506dc0c
Tiago Gonçalves <tiago.miguel.cg@gmail.com>
2019-04-07 11:50:53 +0100

    Revert "Merge branch 'master' of github.com:TM-CG/cnv1819"
    This reverts commit 71212daba90e64f5af862156b389001dafabece0, reversing
    changes made to 6f9790178d2e05dfa02719c7826bc0c075e4610a.

--- compiler.sh
+++
@@ -1,4 +0,0 @@
-javac ~/cnvproject/pt/ulisboa/tencico/cnv/generator
-javac ~/cnvproject/pt/ulisboa/tencico/cnv/server
-javac ~/cnvproject/pt/ulisboa/tencico/cnv/solver
-javac ~/cnvproject/pt/ulisboa/tencico/cnv/util
