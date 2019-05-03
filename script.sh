0387d1314a92470e003055955fe4fa5a71894de1
EC2 Default User <ec2-user@ip-172-31-45-71.eu-central-1.compute.internal>
2019-05-03 12:23:06 +0000

    fixes aws version

--- script.sh
+++ script.sh
@@ -13,9 +13,9 @@ export CNV_TMP=$HOME/compiled_cnv
 export CNV_LOGS=$CNV_ROOT/Logs
 
 # Path to AWS Java SDK
-export AWS_SDK=$HOME/aws-java-sdk-1.11.541
+export AWS_SDK=$HOME/aws-java-sdk-1.11.545
 
-export CLASSPATH=$CNV_ROOT:$CNV_METRICS:$CNV_METRICS/CNVMetric.class:$AWS_SDK/lib/aws-java-sdk-1.11.541.jar:$AWS_SDK/third-party/lib/*:.
+export CLASSPATH=$CNV_ROOT:$CNV_METRICS:$CNV_METRICS/CNVMetric.class:$AWS_SDK/lib/aws-java-sdk-1.11.545.jar:$AWS_SDK/third-party/lib/*:.
 
 # Color codes
 export BLINK_ENABLE="\e[5m"
