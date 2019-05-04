58ce1afde5de4bf0db8692d848d8d8c0d99d3d37
EC2 Default User <ec2-user@ip-172-31-45-71.eu-central-1.compute.internal>
2019-05-04 11:50:07 +0000

    modifies runnable for maven

--- runnable.sh
+++ runnable.sh
@@ -2,5 +2,6 @@
 export CLASSPATH="$CLASSPATH:/home/ec2-user/cnv-project/src/java/"
 source /home/ec2-user/.bashrc
 
-cd /home/ec2-user/cnv-project
-java pt.ulisboa.tecnico.cnv.server.WebServer
+export MAVEN_OPTS="-XX:-UseSplitVerifier"
+cd /home/ec2-user/cnv-project/cnv1819/WebServer
+mvn exec:java
