4c9667b8063f1fbb372db296ac5cdf62ae7232f5
Tiago Gonçalves <tiago.miguel.cg@gmail.com>
2019-05-09 16:08:31 +0100

    fix pom

--- cnv1819/Instrumentation/src/main/java/pt/ulisboa/tecnico/cnv/metrics/LogReader.java
+++ cnv1819/Instrumentation/src/main/java/pt/ulisboa/tecnico/cnv/metrics/LogReader.java
@@ -3,7 +3,7 @@ package pt.ulisboa.tecnico.cnv.metrics;
 import java.io.*;
 
 public class LogReader {
-    public static void main(String argv[]) {
+    public static void main(String[] argv) {
         String fileName = argv[0];
         System.out.println("Reading " + fileName + "...");
         try {
@@ -14,14 +14,14 @@ public class LogReader {
             FileInputStream fi = new FileInputStream(file);
             ObjectInputStream oi = new ObjectInputStream(fi);
             Metrics metrics = (Metrics) oi.readObject();
-	    System.out.printf("# Basic Blocks: %d\n", metrics.basicBlocks());
-	    System.out.printf("# Branches NOT TAKEN: %d\n", metrics.getBranches());
-	    System.out.printf("Map Size (Witdth x Height): (%d x %d)\n", metrics.getWidth(), metrics.getHeight());
-	    System.out.printf("Upper-left corner: (%d , %d)\n", metrics.getX0(), metrics.getY0());
-	    System.out.printf("Lower-right corner: (%d, %d)\n", metrics.getX1(), metrics.getY1());
-	    System.out.printf("Starting point: (%d, %d)\n", metrics.getXS(), metrics.getYS());
+            System.out.printf("# Basic Blocks: %d\n", metrics.basicBlocks());
+            System.out.printf("# Branches NOT TAKEN: %d\n", metrics.getBranches());
+            System.out.printf("Map Size (Witdth x Height): (%d x %d)\n", metrics.getWidth(), metrics.getHeight());
+            System.out.printf("Upper-left corner: (%d , %d)\n", metrics.getX0(), metrics.getY0());
+            System.out.printf("Lower-right corner: (%d, %d)\n", metrics.getX1(), metrics.getY1());
+            System.out.printf("Starting point: (%d, %d)\n", metrics.getXS(), metrics.getYS());
             System.out.printf("Search Algorithm: %s\n", metrics.getAlgorithm());
-	    System.out.printf("Path to map: %s\n", metrics.getMap());
+	        System.out.printf("Path to map: %s\n", metrics.getMap());
 
             oi.close();
         }
