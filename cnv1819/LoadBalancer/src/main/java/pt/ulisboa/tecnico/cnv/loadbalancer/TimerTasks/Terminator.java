b12d831a9905ee81d3f4a3f9b59dace976293e79
Vítor Nunes <vitor.sobrinho.nunes@gmail.com>
2019-05-20 17:48:20 +0100

    Fixes small bugs on Starter and Terminator

--- cnv1819/LoadBalancer/src/main/java/pt/ulisboa/tecnico/cnv/loadbalancer/TimerTasks/Terminator.java
+++ cnv1819/LoadBalancer/src/main/java/pt/ulisboa/tecnico/cnv/loadbalancer/TimerTasks/Terminator.java
@@ -5,6 +5,7 @@ import pt.ulisboa.tecnico.cnv.loadbalancer.LoadBalancer;
 import pt.ulisboa.tecnico.cnv.loadbalancer.InstanceInfo;
 
 import java.util.List;
+import java.util.ArrayList;
 import com.amazonaws.services.ec2.model.InstanceStateChange;
 
 public class Terminator extends GenericTimeTask {
@@ -20,18 +21,24 @@ public class Terminator extends GenericTimeTask {
     @Override
     public void run() {
         List<InstanceStateChange> state;
+        List<InstanceInfo> tmpInstances = new ArrayList<>();
         int stateCode;
         synchronized(loadBalancer.toDeleteLock) {
             for (InstanceInfo instance : loadBalancer.toDelete ) {
                 if (instance.isToDelete() && instance.getJobs().size() == 0) {
                     //terminate instance
-                    state = instanceManager.terminateInstances(loadBalancer.setInstanceForDelete());
+                    state = instanceManager.terminateInstances(instance);
                     stateCode = state.get(0).getCurrentState().getCode();
 
                     if ( stateCode == 32 || stateCode == 48) 
-                        loadBalancer.instanceInfoMap.remove(instance.getInstance().getPublicIpAddress());
+                        tmpInstances.add(instance);
                         
                 }
+            }
+
+            for (InstanceInfo instance : tmpInstances) {
+                loadBalancer.toDelete.remove(instance);
+                loadBalancer.instanceInfoMap.remove(instance.getInstance().getPublicIpAddress());
             }
         }
     }
